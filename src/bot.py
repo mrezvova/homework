1bc5285739e0287f9eee9dd143714059a178369d
mrezvova <rezvmaria@gmail.com>
2024-02-09 21:33:26 +0300

    deploying version

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,7 @@ import requests
 import json
 
 
+from aiogram.fsm.storage.redis import RedisStorage
 from aiogram import Bot, Dispatcher, types, F, Router, html
 from typing import Any, Dict
 from datetime import datetime
@@ -23,7 +24,8 @@ form_router = Router()
 class Form(StatesGroup):
     login = State()
     password = State()
-    user_id = State()
+    person_id = State()
+    jwt_token = State()
 
 
 
@@ -32,16 +34,14 @@ logging.basicConfig(level=logging.INFO)
 # Объект бота
 bot = Bot(token="6906727176:AAGXaFq5xWf_WgTKG90OvBMZgNMXbvsCWZY")
 # Диспетчер
-# redis_client = Redis.from_url("redis://localhost:6379")
-# dp = Dispatcher(storage=RedisStorage(redis=redis_client))
-dp = Dispatcher()
+storage = RedisStorage.from_url("redis://localhost:6379")
 
+dp = Dispatcher(storage=storage)
+# dp = Dispatcher()
 
-    
+#@dp.message(Command("lessons"))
 
-# Хэндлер на команду /start
-@dp.message(Command("lessons"))
-async def cmd_start(message: types.Message, state: FSMContext):
+async def request_data(message: types.Message, state: FSMContext):
     data = await state.get_data()
     login = data["login"]
     password = data["password"]
@@ -51,12 +51,64 @@ async def cmd_start(message: types.Message, state: FSMContext):
     res = requests.post(url, data=json.dumps(payload), headers=headers)
     response_dict = json.loads(res.text)
     token_jwt = response_dict['data']['token']
-
     token = { 'X-Jwt-Token' : token_jwt}
+    print(token)
+    await state.update_data(jwt_token=token)
+
+    url_l = f'https://dnevnik2.petersburgedu.ru/api/journal/institution/related-jurisdiction-list?p_page=1'
+    response = requests.post(url_l, data={}, headers=token)
+    print(type(response.text))
+    first_id = json.loads(response.text)
+    first_id = first_id['data']['items'][0]['id']
+
+    url_l = f'https://dnevnik2.petersburgedu.ru/api/journal/institution/related-institution-list?p_page=1&p_jurisdictions%5B%5D={first_id}'
+    response = requests.post(url_l, data={}, headers=token)
+    second_id = json.loads(response.text)
+    second_id = second_id['data']['items'][0]['id']
+
+
+    url_l = f'https://dnevnik2.petersburgedu.ru/api/journal/group/related-group-list?p_page=1&p_jurisdictions%5B%5D=18&p_institutions%5B%5D={second_id}'
+    response = requests.post(url_l, data={}, headers=token)
+    group_id = json.loads(response.text)
+    group_id = group_id['data']['items'][0]['id']
+
+
+
+    url_l = f'https://dnevnik2.petersburgedu.ru/api/journal/person/related-person-list?p_page=1&p_jurisdictions%5B%5D=18&p_institutions%5B%5D=1519&p_groups%5B%5D={group_id}'
+    response = requests.post(url_l, data={}, headers=token)
+    person_id = json.loads(response.text)
+    person_id = person_id['data']['items'][0]['educations'][0]['education_id']
+
+    await state.update_data(person_id=person_id)
+    await message.answer('Чтобы узнать уроки на сегодня, используй команду /lessons')
+    await cmd_start(message=message, state=state)
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message, state: FSMContext):
+    kb = [
+        [
+            types.KeyboardButton(text="/registration"),
+            types.KeyboardButton(text="/lessons")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+    )
+    await message.answer("Привет! Если ты еще не зашел(а) в свой аккаунт Электронного дневника, то нажми /registration. Если ты уже заходил(а) в свой аккаунт, то нажми /lessons", reply_markup=keyboard)
+
+@dp.message(Command("lessons"))
+async def cmd_start(message: types.Message, state: FSMContext):
+
+    data = await state.get_data()
+    person_id = data["person_id"]
+    jwt_token = data["jwt_token"]
     today = datetime.today()
     datem = today.strftime("%d.%m")
-    url_l = f'https://dnevnik2.petersburgedu.ru/api/journal/lesson/list-by-education?p_datetime_from={datem}.2024%2000:00:00&p_datetime_to={datem}.2024%2023:59:59&p_educations%5B%5D=908910'
-    response = requests.post(url_l, data={}, headers=token)
+    url_l = f'https://dnevnik2.petersburgedu.ru/api/journal/lesson/list-by-education?p_datetime_from={datem}.2024%2000:00:00&p_datetime_to={datem}.2024%2023:59:59&p_educations%5B%5D={person_id}'
+    response = requests.post(url_l, data={}, headers=jwt_token)
     response = json.loads(response.text)
 
     nl = '\n'
@@ -67,9 +119,9 @@ async def cmd_start(message: types.Message, state: FSMContext):
             await message.answer(f'Предмет: {i["subject_name"]}{nl}Домашнего задания нет (¬‿¬ ){nl}')
 
 
-@dp.message(Command("reg"))
+@dp.message(Command("registration"))
 async def login_step(message: types.Message, state: FSMContext):
-    await message.answer(text='Привет! Напиши свой логин(email) без лишних пробелов:')
+    await message.answer(text='Привет! Напиши свой логин(email) от Электронного дневника без лишних пробелов:')
     await state.set_state(Form.login)
 
 
@@ -84,7 +136,7 @@ async def password_step(message: types.Message, state: FSMContext):
 @dp.message(Form.password)
 async def send_step(message: types.Message, state: FSMContext):
     await state.update_data(password=message.text)
-    await cmd_start(message=message, state=state)
+    await request_data(message=message, state=state)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
